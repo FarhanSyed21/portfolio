11104d48f865b94ce6ed8975f490445806b6e721
Syed Farhan <farhansyed0007@gmail.com>
2022-12-28 13:27:32 +0530

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+
+            <p className='text-xl mt-10'>
+                Strong in design and integration with intuitive problem-solving skills. Proficient in JAVA, HTML5, CSS3, JAVASCRIPT, REACT JS, BOOTSTRAP, MATERIAL UI and TAILWIND CSS. Passionate about implementing and launching new projects. Ability to translate business requirements into technical solutions. Looking to start the career as an entry-level software engineer with a reputed firm driven by technology.
+                I'm very enthusiastic about doing work and meeting any kind of deadline.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
